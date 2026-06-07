898a48d881924be88cc9e2b3357483423e4219e5
altradits <altradits@gmail.com>
2026-06-07 14:31:43 +0300

    Add solution.go template files for all challenge folders
    - Created solution.go template in all 131 challenge folders
    - Each solution.go includes TODO comment and basic package structure
    - Provides students with a starting point for their implementations

---
+++ 30-searchreplace/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Write your solution here
+	fmt.Println("Hello, World!")
+}
---
+++ 65-revwstr/solution.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: Write your solution here
+// Follow the instructions in README.md
+
+func main() {
+	// Your code here
+}
