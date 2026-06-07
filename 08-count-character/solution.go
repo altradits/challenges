5729fcf15020e2623faf7c927e3d7da26fb004d5
altradits <altradits@gmail.com>
2026-06-07 19:07:01 +0300

    updat solution.go Count Character

--- 08-count-character/solution.go
+++ 08-count-character/solution.go
@@ -1,8 +1,23 @@
 package main
 
-// TODO: Write your solution here
-// Follow the instructions in README.md
+import "fmt"
+
+func CountChar(str string, c rune) int {
+
+	count := 0
+
+	for _, char := range str {
+
+		if char == c {
+			count++
+		}
+	}
+	return count
+}
 
 func main() {
-	// Your code here
+	fmt.Println(CountChar("Hello World", 'l'))
+	fmt.Println(CountChar("5  balloons", 5))
+	fmt.Println(CountChar("   ", ' '))
+	fmt.Println(CountChar("The 7 deadly sins", '7'))
 }
