799750a64433f47c6b0b074dab6ed653ff6ee00b
altradits <altradits@gmail.com>
2026-06-15 22:41:29 +0300

    backup: altradits vanila

--- 10-findsubstring/solution.go
+++ 10-findsubstring/solution.go
@@ -1,8 +1,27 @@
 package main
 
-// TODO: Write your solution here
-// Follow the instructions in README.md
+import "fmt"
+
+func FindSubString(text, substring string) int {
+	
+	for i, word := range text {
+
+		if len(substring) == 0 {
+			return 0
+		}
+
+		
+
+		if word == substring {
+			return i
+		}
+	}
+}
 
 func main() {
-	// Your code here
+	fmt.Println(FindSubString("Hello World", "World"))
+	fmt.Println(FindSubString("Hello World", "hello"))
+	fmt.Println(FindSubString("Hello World", ""))
+	fmt.Println(FindSubString("Hello World", "xyz"))
+	fmt.Println(FindSubString("banana", "ana"))
 }
